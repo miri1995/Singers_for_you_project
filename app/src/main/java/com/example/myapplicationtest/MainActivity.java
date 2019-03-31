71b0a939ddedd90d0d1fb01b5a7650ae11c868f8
miri1995 <miriasher@gmail.com>
2019-03-31 22:35:26 +0300

    db logic

--- app/src/main/java/com/example/myapplicationtest/MainActivity.java
+++ app/src/main/java/com/example/myapplicationtest/MainActivity.java
@@ -20,7 +20,7 @@ public class MainActivity extends AppCompatActivity {
         // m.main();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        DBConnection.getInstance().getConnection(); // DB connection
+        DBConnection.getInstance(null,null,null).getConnection(); // DB connection
         String text="";
         try {
             InputStream is = getAssets().open("pair3.txt");
