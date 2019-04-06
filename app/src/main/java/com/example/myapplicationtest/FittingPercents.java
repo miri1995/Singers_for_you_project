d6d407847f4ed404d294cd3da94fc5f7aa3484d7
michalBilitzky1 <michalbilitzky1994@gmail.com>
2019-04-06 20:33:52 +0300

    percents

--- app/src/main/java/com/example/myapplicationtest/FittingPercents.java
+++ app/src/main/java/com/example/myapplicationtest/FittingPercents.java
@@ -3,27 +3,55 @@ package com.example.myapplicationtest;
 import com.example.myapplicationtest.SingersLogic.Priority;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 public class FittingPercents {
     private Priority priority;
 
-    public FittingPercents(Priority priority){
+    public FittingPercents(Priority priority) {
         this.priority = priority;
     }
 
-    public List<Double> percent(){
+    public List<Double> percent() {
         List<Double> gradesLoudness = new ArrayList<>();
-        int gradeTempo;
-        if(priority.getPrioLoudness().equals("low")){
-            if(priority.getFilters().getLoudness().equals("Weak"))
-            for(int i=0;i<SulationSinger.loudness.size();i++){
-                float step = Math.abs(Integer.parseInt(SulationSinger.loudness.get(i))-(-16));
-                double gradeLoudness = 100 * (1-step) * 0.3;
-                gradesLoudness.add(gradeLoudness);
+        List<Double> gradesTempo = new ArrayList<>();
+        double gradeTempo;
+        double gradeLoudness;
+        if (priority.getFilters().getTempo().equals("Fast") || priority.getFilters().getTempo().equals("Slow")) {
+            if (priority.getFilters().getLoudness().equals("Weak") || priority.getFilters().getTempo().equals("Strong")) {
+                float numLoudness[] = Maps.getInstance().PutInloudness(priority.getFilters().getLoudness());
+                HashMap<String, Float> priorityMap = Maps.getInstance().PutInPriority(priority.getPrioLoudness(), priority.getPrioTempo());
+                double percentLoudness = Maps.getInstance().PutInPercents(priority.getPrioLoudness());
+                float numTempo[] = Maps.getInstance().PutInTempo(priority.getFilters().getTempo());
+                double percentTempo = Maps.getInstance().PutInPercents(priority.getPrioTempo());
+                for (int i = 0; i < SulationSinger.loudness.size(); i++) {
+                    float step = (Math.abs(Integer.parseInt(SulationSinger.loudness.get(i)) - numLoudness[0])) / priorityMap.get(priority.getPrioLoudness());
+                    gradeLoudness = 100 * (1 - step) * percentLoudness;
+                    gradesLoudness.add(gradeLoudness);
+                }
+                for (int i = 0; i < SulationSinger.loudness.size(); i++) {
+                    float step = (Math.abs(Integer.parseInt(SulationSinger.tempo.get(i)) - numTempo[0])) / priorityMap.get(priority.getPrioTempo());
+                    gradeTempo = 100 * (1 - step) * percentTempo;
+                    gradesTempo.add(gradeTempo);
+                }
             }
+        }
+       /* else{
+
+        }*/
+
+
+
+
+
+     /*   if(priority.getPrioLoudness().equals("low")){
+            if(priority.getFilters().getLoudness().equals("Weak"))
+
 
         }
         return gradesLoudness;
+    }*/
+     return gradesLoudness;
     }
 }
--- app/src/main/java/com/example/myapplicationtest/Maps.java
+++ app/src/main/java/com/example/myapplicationtest/Maps.java
@@ -8,59 +8,60 @@ import java.util.Map;
 public class Maps {
 
     private static Maps single_instance = null;
-    HashMap<String, Integer> priority = new HashMap<>();
-    HashMap<String, Integer> percents = new HashMap<>();
+    HashMap<String, Float> priority = new HashMap<>();
+  //  HashMap<String, Float> percents = new HashMap<>();
+    //HashMap<String,Integer> loudnessMap = new HashMap<>();
+    //HashMap<String,Integer>tempoMap = new HashMap<>();
 
 
-    private Maps(){
+    private Maps() {
 
     }
 
-    public Map<String, Integer> getMapPrio(){
+    public Map<String, Float> getMapPrio() {
 
         return priority;
     }
 
-    public Map<String, Integer> getMapPercents(){
+   /* public Map<String, Float> getMapPercents() {
 
         return percents;
-    }
+    }*/
 
-    public static Maps getInstance()
-    {
+    public static Maps getInstance() {
         if (single_instance == null)
             single_instance = new Maps();
 
         return single_instance;
     }
 
-    public HashMap<String,Integer> PutInPriority (String prioLoudness, String prioTempo){
-        switch(prioLoudness){
+    public HashMap<String, Float> PutInPriority(String prioLoudness, String prioTempo) {
+        switch (prioLoudness) {
             case "high":
-                priority.put(prioLoudness,0);
-                if(prioTempo.equals("medium")){
-                    priority.put(prioTempo,25);
+                priority.put(prioLoudness, (float) 0);
+                if (prioTempo.equals("medium")) {
+                    priority.put(prioTempo, (float) 25);
                 }
-                if(prioTempo.equals("low")){
-                    priority.put(prioTempo,50);
+                if (prioTempo.equals("low")) {
+                    priority.put(prioTempo, (float) 50);
                 }
                 break;
             case "medium":
-                priority.put(prioLoudness,5);
-                if(prioTempo.equals("high")){
-                    priority.put(prioTempo,0);
+                priority.put(prioLoudness, (float) 5);
+                if (prioTempo.equals("high")) {
+                    priority.put(prioTempo, (float) 0);
                 }
-                if(prioTempo.equals("low")){
-                    priority.put(prioTempo,50);
+                if (prioTempo.equals("low")) {
+                    priority.put(prioTempo, (float) 50);
                 }
                 break;
             case "low":
-                priority.put(prioLoudness,10);
-                if(prioTempo.equals("high")){
-                    priority.put(prioTempo,0);
+                priority.put(prioLoudness, (float) 10);
+                if (prioTempo.equals("high")) {
+                    priority.put(prioTempo, (float) 0);
                 }
-                if(prioTempo.equals("medium")){
-                    priority.put(prioTempo,25);
+                if (prioTempo.equals("medium")) {
+                    priority.put(prioTempo, (float) 25);
                     //probably something else
                 }
                 break;
@@ -70,4 +71,48 @@ public class Maps {
 
     }
 
+    public float[] PutInloudness(String loudness) {
+        float numLoud[] = new float[2];
+        switch (loudness) {
+            case "Weak":
+                numLoud[0] = -16;
+                break;
+            case "Normal":
+                numLoud[0] = -32;
+                numLoud[1] = -16;
+                break;
+            case "Strong":
+                numLoud[0] = -32;
+        }
+        return numLoud;
+    }
+
+    public float[] PutInTempo(String tempo) {
+        float numTempo[] = new float[2];
+        switch (tempo) {
+            case "Slow":
+                numTempo[0] = 85;
+                break;
+            case "Medium":
+                numTempo[0] = 85;
+                numTempo[1] = 170;
+                break;
+            case "Fast":
+                numTempo[0] = 170;
+        }
+        return numTempo;
+    }
+
+    public double PutInPercents(String whichPrio){
+        double whichPercent = 0;
+        switch (whichPrio){
+            case "low":
+                whichPercent = 0.3;
+                break;
+            case "medium":
+                whichPercent = 0.7;
+
+        }
+        return whichPercent;
+    }
 }
