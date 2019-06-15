66e801ee6563c3f166b23a5584472a1654ea7477
miri1995 <miriasher@gmail.com>
2019-06-15 21:09:43 +0300

    design

--- app/src/main/java/com/example/myapplicationtest/HelperLists.java
+++ app/src/main/java/com/example/myapplicationtest/HelperLists.java
@@ -339,7 +339,7 @@ public class HelperLists {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
                         dialog.cancel();
-                        
+
 
                     }
                 });
