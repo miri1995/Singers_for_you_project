c73618af9942fcade653aa9086c5e80f5afd483a
miri1995 <miriasher@gmail.com>
2019-06-21 11:03:54 +0300

    dbconfig

--- app/src/main/java/com/example/ArtistsForYou/YouTube/YouTubeConfig.java
+++ app/src/main/java/com/example/ArtistsForYou/YouTube/YouTubeConfig.java
@@ -3,8 +3,8 @@ package com.example.ArtistsForYou.YouTube;
 
 public class YouTubeConfig {
 
-    private static final String API_KEY="AIzaSyCh-7diJlKRcvzPhrR57ojg2Swe0OJaHUE";
-
+   // private static final String API_KEY="AIzaSyCh-7diJlKRcvzPhrR57ojg2Swe0OJaHUE";
+    private static final String API_KEY="";
     public YouTubeConfig(){
 
     }
